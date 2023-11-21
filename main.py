8c220fd1edaf3385aefe1ce660c12c1f2fe2515b
ret7020 <ret7020@gmail.com>
2023-11-21 17:07:24 +0300

    Base version, python multiprocessing

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from flask_sock import Sock
 import logging
 import cv2
 import numpy as np
-# import threading
 import time
 import sys
 import gzip
 import base64
+from multiprocessing import Process, Queue
 
 class Camera:
     def __init__(self, index) -> None:
@@ -16,11 +16,8 @@ class Camera:
         self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         self.encode_params = [int(cv2.IMWRITE_JPEG_QUALITY), 70]
         self.image = cv2.imencode('.jpg', np.zeros((480, 640)), self.encode_params)[1].tobytes()
+        self.image = gzip.compress(self.image)
         logging.info("Camera inited")
-        _, self.cache = self.camera.read()
-        self.cache = cv2.cvtColor(self.cache, cv2.COLOR_BGR2GRAY)
-        self.cache = cv2.imencode('.jpg', self.cache, self.encode_params)[1].tobytes()
-        self.cache = gzip.compress(self.cache)
         
 
     def get_jpeg_image_bytes(self):
@@ -33,6 +30,10 @@ class Camera:
         res = gzip.compress(res)
         
         return res
+    
+    def loop(self, image_share):
+        while True:
+            image_share.put(self.get_jpeg_image_bytes())
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -40,20 +41,21 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 sock = Sock(app)
 #camera = Camera("file_example_MP4_1920_18MG.mp4")
 camera = Camera(0)
+image_share = Queue()
+p = Process(target=camera.loop, args=(image_share,))
+p.start()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-@app.route('/api/control', methods=['POST'])
-def api_manual_control():
-    pass
-
+@app.route('/stop')
+def stop_api():
+    p.terminate()
 
 @sock.route('/image')
 def stream(sock):
     logging.error("Stream started")
     while True:
-        sock.send(camera.get_jpeg_image_bytes())
+        sock.send(image_share.get())
         
